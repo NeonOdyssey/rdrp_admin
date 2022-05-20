93f2b4edfefcb2866d4b4b7ad97bb0c6fd51a5b0
btwentyone <35628565+btwentyone@users.noreply.github.com>
2022-05-20 21:31:16 +0200

    Add heal to redemrp_status

--- scripts/client/commands/misc.lua
+++ scripts/client/commands/misc.lua
@@ -1,6 +1,6 @@
 RegisterNUICallback('heal', function(data, cb)
     data.staff = GetPlayerName(PlayerId())
-
+    TriggerServerEvent('rdrp_admin:heal', source, data, Config.Perms.Heal)
 end)
 
 RegisterNUICallback('spectate', function(data, cb)
